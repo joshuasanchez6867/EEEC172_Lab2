0f8a3adda5ce8a457945164db8ccd47ae4c77b61
pandabreeder <jossanchez@ucdavis.edu>
2022-04-12 18:27:56 -0700

    first commit

---
+++ main.c
@@ -0,0 +1,244 @@
+//*****************************************************************************
+//
+// Copyright (C) 2014 Texas Instruments Incorporated - http://www.ti.com/ 
+// 
+// 
+//  Redistribution and use in source and binary forms, with or without 
+//  modification, are permitted provided that the following conditions 
+//  are met:
+//
+//    Redistributions of source code must retain the above copyright 
+//    notice, this list of conditions and the following disclaimer.
+//
+//    Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the 
+//    documentation and/or other materials provided with the   
+//    distribution.
+//
+//    Neither the name of Texas Instruments Incorporated nor the names of
+//    its contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+//  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+//  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//*****************************************************************************
+
+//*****************************************************************************
+//
+// Application Name     - SPI Demo
+// Application Overview - The demo application focuses on showing the required 
+//                        initialization sequence to enable the CC3200 SPI 
+//                        module in full duplex 4-wire master and slave mode(s).
+//
+//*****************************************************************************
+
+
+//*****************************************************************************
+//
+//! \addtogroup SPI_Demo
+//! @{
+//
+//*****************************************************************************
+
+// Standard includes
+#include <string.h>
+
+// Driverlib includes
+#include "hw_types.h"
+#include "hw_memmap.h"
+#include "hw_common_reg.h"
+#include "hw_ints.h"
+#include "spi.h"
+#include "rom.h"
+#include "rom_map.h"
+#include "utils.h"
+#include "prcm.h"
+#include "uart.h"
+#include "interrupt.h"
+
+// Common interface includes
+#include "uart_if.h"
+#include "pin_mux_config.h"
+#include "test.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1351.h"
+
+
+#define APPLICATION_VERSION     "1.4.0"
+
+#define SPI_IF_BIT_RATE  100000
+#define TR_BUFF_SIZE     100
+
+//*****************************************************************************
+//                 GLOBAL VARIABLES -- Start
+//*****************************************************************************
+
+
+#if defined(ccs)
+extern void (* const g_pfnVectors[])(void);
+#endif
+#if defined(ewarm)
+extern uVectorEntry __vector_table;
+#endif
+//*****************************************************************************
+//                 GLOBAL VARIABLES -- End
+//*****************************************************************************
+
+
+//
+// Initialize SPI
+// - Makes application ready for SPI communication
+//
+void InitSPI() {
+    //
+    // Reset SPI
+    //
+    MAP_SPIReset(GSPI_BASE);
+
+    //
+    // Configure SPI interface
+    //
+    MAP_SPIConfigSetExpClk(GSPI_BASE,MAP_PRCMPeripheralClockGet(PRCM_GSPI),
+                         SPI_IF_BIT_RATE,SPI_MODE_MASTER,SPI_SUB_MODE_0,
+                         (SPI_SW_CTRL_CS |
+                         SPI_4PIN_MODE |
+                         SPI_TURBO_OFF |
+                         SPI_CS_ACTIVEHIGH |
+                         SPI_WL_8));
+
+    //
+    // Enable SPI for communication
+    //
+    MAP_SPIEnable(GSPI_BASE);
+}
+
+//
+// OLED Main Routine
+// - Main code for the OLED display routine
+//
+// ROUTINE Specification
+//  1. Print the full character-set found in the font table in glcdfont.h.
+//  2. Print the string “Hello world!” to the display
+//  3. Display a pattern of 8 bands of different colors horizontally across the full OLED display.
+//  4. Display a pattern of 8 bands of different colors vertically across the full OLED display.
+//  5. Call the testlines() function to display diagonal lines.
+//  6. Call the testfastlines() function to display a rectangular grid pattern.
+//  7. Call the testdrawrects() function to draw a pattern of rectangles.
+//  8. Call the testfillrects() function.
+//  9. Call the testfillcircles() and testdrawcircles() functions.
+// 10. Call the testroundrects() function.
+// 11. Call the testtriangles() function.
+//
+#define WHITE           0xFFFF
+#define RED             0xF800
+void OLEDMain() {
+    InitSPI();
+    Adafruit_Init();
+
+    while (1) {
+        Message("Starting OLED Routine\r\n");
+        delay(100);
+
+        fillScreen(WHITE);
+        fillScreen(RED);
+    }
+}
+
+
+//*****************************************************************************
+//
+//! Board Initialization & Configuration
+//!
+//! \param  None
+//!
+//! \return None
+//
+//*****************************************************************************
+static void
+BoardInit(void)
+{
+/* In case of TI-RTOS vector table is initialize by OS itself */
+#ifndef USE_TIRTOS
+  //
+  // Set vector table base
+  //
+#if defined(ccs)
+    MAP_IntVTableBaseSet((unsigned long)&g_pfnVectors[0]);
+#endif
+#if defined(ewarm)
+    MAP_IntVTableBaseSet((unsigned long)&__vector_table);
+#endif
+#endif
+    //
+    // Enable Processor
+    //
+    MAP_IntMasterEnable();
+    MAP_IntEnable(FAULT_SYSTICK);
+
+    PRCMCC3200MCUInit();
+}
+
+//*****************************************************************************
+//
+//! Main function for spi demo application
+//!
+//! \param none
+//!
+//! \return None.
+//
+//*****************************************************************************
+void main()
+{
+    //
+    // Initialize Board configurations
+    //
+    BoardInit();
+
+    //
+    // Muxing UART and SPI lines.
+    //
+    PinMuxConfig();
+
+    //
+    // Enable the SPI module clock
+    //
+    MAP_PRCMPeripheralClkEnable(PRCM_GSPI,PRCM_RUN_MODE_CLK);
+
+    //
+    // Initialising the Terminal.
+    //
+    InitTerm();
+
+    //
+    // Clearing the Terminal.
+    //
+    ClearTerm();
+
+    //
+    // Display the Banner
+    //
+    Message("\n\n\n\r");
+    Message("\t\t   ********************************************\n\r");
+    Message("\t\t        CC3200 OLED Demo Application  \n\r");
+    Message("\t\t   ********************************************\n\r");
+    Message("\n\n\n\r");
+
+    //
+    // Reset the peripheral
+    //
+    MAP_PRCMPeripheralReset(PRCM_GSPI);
+
+    OLEDMain();
+
+}
+
---
+++ test.h
@@ -0,0 +1,16 @@
+#ifndef __TEST_H__
+#define __TEST_H__
+
+void delay(unsigned long ulCount);
+void testfastlines(unsigned int color1, unsigned int color2);
+void testdrawrects(unsigned int color);
+void testfillrects(unsigned int color1, unsigned int color2);
+void testfillcircles(unsigned char radius, unsigned int color);
+void testdrawcircles(unsigned char radius, unsigned int color);
+void testtriangles();
+void testroundrects();
+void testlines(unsigned int color);
+void lcdTestPattern(void);
+void lcdTestPattern2(void);
+
+#endif //  __TEST_H__
